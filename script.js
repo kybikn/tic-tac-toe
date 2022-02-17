9a01e853878356fe8058fe4ec4b23c64d4ff2e44
Kybik <82117054+kybikn@users.noreply.github.com>
2022-02-17 22:49:46 +0300

    feat: add audio

--- script.js
+++ script.js
@@ -39,7 +39,9 @@ function win() {
         box[comb[i][0]].classList.add("active");
         box[comb[i][1]].classList.add("active");
         box[comb[i][2]].classList.add("active");
-        res.innerText = "Выиграли X";
+        res.innerText = "Выиграли крестики!  Ходов: " + count;
+        let crossAudio = new Audio("./audio/audio.mp3");
+        crossAudio.play();
       }, 1000);
       game.removeEventListener("click", init);
     } else if (
@@ -51,11 +53,15 @@ function win() {
         box[comb[i][0]].classList.add("active");
         box[comb[i][1]].classList.add("active");
         box[comb[i][2]].classList.add("active");
-        res.innerText = "Выиграли O";
+        res.innerText = "Выиграли нолики!  Ходов: " + count;
+        let crossAudio = new Audio("./audio/audio.mp3");
+        crossAudio.play();
       }, 1000);
       game.removeEventListener("click", init);
     } else if (count == 9) {
-      res.innerText = "Ничья";
+      res.innerText = "Ничья!  Ходов: " + count;
+      let crossAudio = new Audio("./audio/gameOver.mp3");
+      crossAudio.play();
       game.removeEventListener("click", init);
     }
   }
@@ -68,8 +74,14 @@ function newGame() {
   box.forEach((item) => {
     item.innerHTML = "";
     item.classList.remove("X", "O", "active");
+    let crossAudio = new Audio("./audio/start.mp3");
+    crossAudio.play();
   });
+
   game.addEventListener("click", init);
 }
 
-//   let crossAudio = new Audio('audio/cross.mp3');
+// let p1Counter = 1;
+// let p2Counter = 1;
+// const player1 = new Player();
+// const player2 = new Player();
